26fc242e1f53580489d4b6289193bf0ac848f859
Ailton Martins <pinho123@gmail.com>
2026-05-17 23:52:43 -0300

    feat: inicializa transaction-service com Spring Boot, configurações do Gradle, Flyway, ambiente de testes com H2 e integração ao docker-compose

---
+++ transaction-service/src/test/java/com/ailtonmartins/transactionservice/TransactionServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ailtonmartins.transactionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class TransactionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
